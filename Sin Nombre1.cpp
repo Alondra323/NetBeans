9316cf64b57d48a4ff37565928f61f0a5c870034
Alondra323 <149427163+Alondra323@users.noreply.github.com>
2023-11-01 13:39:18 -0500

    Add files via upload

---
+++ Sin Nombre1.cpp
@@ -0,0 +1,23 @@
+PRACTICA 9
+package practica9;
+import java.swing.JOptionPane;
+public class Practica9{
+	public static void main(String[]args)}
+	  int semaforo;
+	do{
+	 semaforo=Integer.parseInt(JOptionPane.showInputDialog(¨
+	 Seleccione el color del semaforo:/a1.-Verde/a2.-Amarillo/a3.-Rojo¨));
+	 switch(semaforo){
+	 	case 1:
+	 		 JOptionPane.showMessageDialog(null,¨Avanzar¨);
+	 		break;
+	 	case 2:
+		     JOptionPane.showMessageDialog(null,¨Avanzar con precaucion¨);
+	 		break;
+		case 3:
+		     JOptionPane.showMessageDialog(null,¨Detengase¨);
+	 		break; 	
+	 }	
+	 } while (semaforol=3);
+   }
+   }
